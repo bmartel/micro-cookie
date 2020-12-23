212946cc711bd651e5945f519d7bf59c06cd6d33
Brandon Martel <brandonmartel@gmail.com>
2020-12-22 22:40:08 -0600

    updating and ensuring cookies object is specified even in error or no headers

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { parse } = require("cookie");
 
-module.exports = handler => (req, res) => {
+module.exports = (handler) => (req, res) => {
   if (req.headers && req.headers.cookie) {
     try {
       req.cookies = parse(req.headers.cookie);
     } catch (err) {
+      req.cookies = {};
       console.warn("Could not parse cookie", req.headers.cookie);
       console.error(err);
     }
+  } else {
+    req.cookies = {};
   }
 
   return handler(req, res);
